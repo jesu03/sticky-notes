2cbf7eb2762109adda38fccf6f36f157181182d0
jesu03 <95433671+jesu03@users.noreply.github.com>
2023-08-04 15:56:53 +0530

    change some colors in the notes node

--- notesapp/src/Note.js
+++ notesapp/src/Note.js
@@ -23,14 +23,16 @@ const Note = (props) => {
  }
   return (
     <div className='note'>
+      <h1 style={{color:'white',fontFamily: 'Arial,Helvetica,sans-serif'}}>Notes App</h1>
+
         <div className='note__content'>
         <div class="md-form form-lg" className='fir'>
-        <input type="text" id="inputLGEx" name="title" value={show.title} class="form-control form-control-lg" onChange={handleClick} />
+        <input type="text" id="inputLGEx" name="title" value={show.title} class="form-control form-control-lg" onChange={handleClick} placeholder="...title"  />
         </div>
         <div class="md-form mb-4 pink-textarea active-pink-textarea" className="sec" >
-        <textarea id="form18" name="content" value={show.content} class="md-textarea form-control" rows="3"  onChange={handleClick}/>
+        <textarea id="form18" name="content" value={show.content} class="md-textarea form-control" rows="3"  onChange={handleClick} placeholder="...description" />
         </div>  
-        <button type="submit" class="btn btn-primary mb-2" onClick={handleNa}>Submit</button>
+        <button type="submit" class="btn btn-primary mb-2" onClick={handleNa}>Save</button>
         </div>
     </div>
   )
--- notesapp/src/Result.js
+++ notesapp/src/Result.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import './Result.css'
-import {MdDeleteForever} from  'react-icons/md'
+// import {MdDeleteForever} from  'react-icons/md'
 const Result = (props) => {
     
     return(
@@ -9,7 +9,7 @@ const Result = (props) => {
         <h3>{props.title}</h3>
         <h5>{props.content}</h5>
         <small>03-08-2023</small>
-         <MdDeleteForever className='delete-icon  />
+         {/* <MdDeleteForever className='delete-icon  /> */}
         </div>
         </div>
     )    
